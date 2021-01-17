f692f516cfe3c1304b6eb856289a050aaef1b209
vencax <vencax@centrum.cz>
2021-01-17 12:04:04 +0100

    fix

--- server.js
+++ server.js
@@ -5,16 +5,21 @@ import bodyParser from 'body-parser'
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
 const app = express()
-const SECRET = process.SERVER_SECRET
+const SECRET = process.env.SERVER_SECRET
 
 app.post('/sign', bodyParser.json(), (req, res, next) => {
-  jwt.sign(req.body, SECRET, { expiresIn: '6h' })
+  jwt.sign(req.body, SECRET, { expiresIn: '6h' }, (err, token) => {
+    return err ? next(err) : res.json({ token })
+  })
 })
 
 app.get('/verify/:token', (req, res, next) => {
   jwt.verify(req.params.token, SECRET, (err, decoded) => {
     return err ? next(err) : res.json(decoded)
   })
+})
+
+app.post('/logout', (req, res, next) => {
 })
 
 app.listen(port, host, (err) => {
