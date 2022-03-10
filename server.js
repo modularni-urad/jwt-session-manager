7a824a70a06a543657e3f8ec7d0e4bb51fd7136d
vencax <vencax@centrum.cz>
2022-03-10 08:57:09 +0100

    bodyparser removed

--- server.js
+++ server.js
@@ -1,26 +1,30 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
-import bodyParser from 'body-parser'
 
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
 const app = express()
 const SECRET = process.env.SERVER_SECRET
+const JSONBodyParser = express.json()
 
-app.post('/sign', bodyParser.json(), (req, res, next) => {
+app.post('/sign', JSONBodyParser, (req, res, next) => {
   jwt.sign(req.body, SECRET, { expiresIn: '6h' }, (err, token) => {
     return err ? next(err) : res.json({ token })
   })
 })
 
-app.get('/verify/:token', (req, res, next) => {
-  jwt.verify(req.params.token, SECRET, (err, decoded) => {
+app.post('/verify', JSONBodyParser, (req, res, next) => {
+  jwt.verify(req.body.token, SECRET, (err, decoded) => {
     return err ? next(err) : res.json(decoded)
   })
 })
 
 app.post('/logout', (req, res, next) => {
   res.json({ status: 'ok' })
+})
+
+app.use((err, req, res, next) => {
+  res.status(400).send(err.message || err)
 })
 
 app.listen(port, host, (err) => {
